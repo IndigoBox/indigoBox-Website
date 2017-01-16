04ab48dad6457e31da6342eebf4766e40d7f42ef
Viktor Koves <viktorkoves@gmail.com>
2017-01-15 22:57:44 -0600

    Moved indigoBox header out of javascript and into Jekyll This is also related to Issue #6, and should help with SEO

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
 $(document).ready( function()
 {
-    //Put in the header
-    $("body").prepend(''
-        + '<nav>'
-        + '<a id="logo" href="index.html"> indigo<span>Box</span></a>'
-
-        + '<ul class="menu">'
-        + '<li id="home" class="selected"><a href="index.html">Home</a></li>'
-        + '<li id="about"><a href="about.html">About</a></li>'
-        + '<li id="projects"><a href="projects/index.html">Projects</a>'
-        + '<ul>'
-        + '<li><a href="snowshoes.html">Snowshoes</a></li>'
-        + '<li><a href="carpe.html">Carpe</a></li>'
-        + '<li><a href="scroll-stop.html">Scroll Stop</a></li>'
-        + '<li><a href="lines.html">Lines</a></li>'
-        + '</ul>'
-        + '</li>'
-        + '<li id="contact"><a href="contact.html">Contact</a></li>'
-        + '</ul>'
-        + '</nav>'
-        + '');
-
     if(window.location.href.indexOf("indigobox.us") > -1) //if production
     {
         $("a").each(function() //iterate through all links
